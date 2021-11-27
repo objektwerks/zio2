1ebec22aefdeb019a7d8040ac1a11684312997c2
objektwerks <objektwerks@runbox.com>
2021-11-27 07:59:12 -0500

    reordered imports

--- src/test/scala/objektwerks/QueueTest.scala
+++ src/test/scala/objektwerks/QueueTest.scala
@@ -1,8 +1,8 @@
 package objektwerks
 
 import zio.Queue
-import zio.test.Assertion.equalTo
 import zio.test._
+import zio.test.Assertion.equalTo
 
 object QueueTest extends ZioTest:
   def spec: Spec[Environment, TestFailure[Nothing], TestSuccess] = suite("queue.test")(
