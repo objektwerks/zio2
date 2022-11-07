ab2e705c24c295ab6dd6cba9f2e3c31114f5fbbb
objektwerks <objektwerks@runbox.com>
2022-11-07 06:54:23 -0500

    enhanced http app

--- src/main/scala/objektwerks/HttpApp.scala
+++ src/main/scala/objektwerks/HttpApp.scala
@@ -7,9 +7,16 @@ import zhttp.http.*
 import zhttp.http.Http
 import zhttp.service.Server
 
-object HttpApp extends ZIOAppDefault:  
+object HttpApp extends ZIOAppDefault:
+  val port = 7272
+
   val router: Http[Any, Nothing, Request, Response] = Http.collect[Request] {
     case Method.GET -> !! / "now" => Response.text(Instant.now.toString())
   }
 
-  override def run = Server.start(port = 7272, http = router)
+  val server = for {
+    _ <- Console.printLine(s"Http server started at http://localhost:$port")
+    _ <- Server.start(port, router)
+  } yield()
+
+  override def run = server
