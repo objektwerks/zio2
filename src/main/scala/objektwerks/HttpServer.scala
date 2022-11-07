8218fd5847fb77e516a8edfef7f0ace4314fd99e
objektwerks <objektwerks@runbox.com>
2022-11-07 18:43:21 -0500

    added fault event; implementing command route

--- src/main/scala/objektwerks/HttpServer.scala
+++ src/main/scala/objektwerks/HttpServer.scala
@@ -6,12 +6,24 @@ import zio.{ Console, ZIO, ZIOAppDefault }
 import zhttp.http.{ !!, /, ->, Http, Method, Request, Response }
 import zhttp.service.Server
 
+import zio.json.{ DecoderOps, EncoderOps }
+
+import Command.given
+import Event.given
+
 object HttpServer extends ZIOAppDefault:
   val port = 7272
 
   val router = Http.collectZIO[Request] {
     case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
     case request @ Method.POST -> !! / "greeting" => request.body.asString.map { name => Response.text(s"\nGreetings, $name!\n") }
+    case request @ Method.POST -> !! / "command" => request.body.asString.map { json =>
+      json.fromJson[Command] match
+        case Right(command) => command match
+          case Add(x, y) => Response.json( Added( x + y ).toJson )
+          case Multiply(x, y) => Response.json( Muliplied( x * y ).toJson )
+        case Left(error) => Response.json( Fault( error ).toJson )
+    }
   }
 
   val server = for {
