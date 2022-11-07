8c813d8e4283c3177c0572b7b688fcc8af71ba95
objektwerks <objektwerks@runbox.com>
2022-11-07 17:22:09 -0500

    added json support to command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -1,5 +1,11 @@
 package objektwerks
 
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+
 sealed trait Command
 
-final case class Double(value: Double) extends Command
+final case class Triple(value: Int) extends Command
+
+object Triple:
+  implicit val decoder: JsonDecoder[Triple] = DeriveJsonDecoder.gen[Triple]
+  implicit val encoder: JsonEncoder[Triple] = DeriveJsonEncoder.gen[Triple]
