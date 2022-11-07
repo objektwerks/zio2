583131c2bb3169460da8a57dfaafc80ea7d16800
objektwerks <objektwerks@runbox.com>
2022-11-07 06:23:54 -0500

    refactored files

--- src/main/scala/objektwerks/Files.scala
+++ src/main/scala/objektwerks/Files.scala
@@ -5,12 +5,14 @@ import scala.io.{BufferedSource, Codec, Source}
 import zio.{Task, ZIO}
 
 object Files {
-  def open(file: String): ZIO[Any, Exception, String] =
-    ZIO
-      .attempt(Source.fromFile(file, Codec.UTF8.name))
-      .acquireReleaseWith(close(_).ignore)(source => read(source))
+  def open(file: String): Task[String] =
+    ZIO.scoped {
+      ZIO.acquireReleaseWith(
+        ZIO.attemptBlocking(Source.fromFile(file))
+      )(file => close(file).orDie) { source => read(source) }
+    }
 
-  def read(source: BufferedSource): Task[String] = ZIO.attempt(source.mkString)
+  def read(source: BufferedSource): Task[String] = ZIO.attemptBlocking(source.mkString)
 
   def close(source: BufferedSource): Task[Unit] = ZIO.attempt(source.close)
 
