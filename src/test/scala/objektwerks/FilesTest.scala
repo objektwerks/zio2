f0b10629d57ca60d450d4dd7a205c4c764f8fda5
objektwerks <objektwerks@runbox.com>
2022-11-07 16:59:40 -0500

    enhanced files test

--- src/test/scala/objektwerks/FilesTest.scala
+++ src/test/scala/objektwerks/FilesTest.scala
@@ -5,6 +5,6 @@ import zio.test._
 object FilesTest extends ZIOSpecDefault:
   def spec = suite("file")(
     test("open") {
-      Files.open("./LICENSE").map(content => assertTrue( content.nonEmpty ))
+      Files.open("./LICENSE").map(content => assertTrue( content.length() == 9125 ))
     }
   )
