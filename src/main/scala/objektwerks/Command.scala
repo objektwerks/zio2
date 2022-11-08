d9b1251d5136d794cebe83bdd12d539c800abc92
objektwerks <objektwerks@runbox.com>
2022-11-07 19:07:13 -0500

    enhanced json codecs

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -8,5 +8,11 @@ case class Add(x: Int, y: Int) extends Command
 case class Multiply(x: Int, y: Int) extends Command
 
 object Command:
-  given decoder: JsonDecoder[Command] = DeriveJsonDecoder.gen[Command]
-  given encoder: JsonEncoder[Command] = DeriveJsonEncoder.gen[Command]
+  given JsonDecoder[Command] = DeriveJsonDecoder.gen[Command]
+  given JsonEncoder[Command] = DeriveJsonEncoder.gen[Command]
+
+  given JsonDecoder[Add] = DeriveJsonDecoder.gen[Add]
+  given JsonEncoder[Add] = DeriveJsonEncoder.gen[Add]
+
+  given JsonDecoder[Multiply] = DeriveJsonDecoder.gen[Multiply]
+  given JsonEncoder[Multiply] = DeriveJsonEncoder.gen[Multiply]
--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -9,5 +9,14 @@ case class Muliplied(z: Int) extends Event
 case class Fault(cause: String) extends Event
 
 object Event:
-  given decoder: JsonDecoder[Event] = DeriveJsonDecoder.gen[Event]
-  given encoder: JsonEncoder[Event] = DeriveJsonEncoder.gen[Event]
+  given JsonDecoder[Event] = DeriveJsonDecoder.gen[Event]
+  given JsonEncoder[Event] = DeriveJsonEncoder.gen[Event]
+
+  given JsonDecoder[Added] = DeriveJsonDecoder.gen[Added]
+  given JsonEncoder[Added] = DeriveJsonEncoder.gen[Added]
+
+  given JsonDecoder[Muliplied] = DeriveJsonDecoder.gen[Muliplied]
+  given JsonEncoder[Muliplied] = DeriveJsonEncoder.gen[Muliplied]
+
+  given JsonDecoder[Fault] = DeriveJsonDecoder.gen[Fault]
+  given JsonEncoder[Fault] = DeriveJsonEncoder.gen[Fault]
