1f7a9ec3dc1c095a4b224269c528c6d0a724ccde
objektwerks <objektwerks@runbox.com>
2022-11-07 19:00:25 -0500

    still sorting out zio-json

--- src/test/scala/objektwerks/JsonTest.scala
+++ src/test/scala/objektwerks/JsonTest.scala
@@ -3,18 +3,17 @@ package objektwerks
 import zio.json.{ DecoderOps, EncoderOps }
 import zio.test.{ assertTrue, ZIOSpecDefault }
 
+import Command.given
+import Event.given
+
 object JsonTest extends ZIOSpecDefault:
   def spec = suite("json")(
     test("command") {
-      import Command.*
-
       val command: Command = Add(1, 1)
       val commandJson = command.toJson
       assertTrue( command == commandJson.fromJson[Command].getOrElse( Add(0, 0) ) )
     },
     test("event") {
-      import Event.*
-
       val event: Event = Added(2)
       val eventJson = event.toJson
       assertTrue( event == eventJson.fromJson[Event].getOrElse( Added(-1) ) )
