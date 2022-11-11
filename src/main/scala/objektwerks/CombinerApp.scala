2f14bbc1b16a786e5eade65b3c4ef8d413e29cc0
objektwerks <objektwerks@runbox.com>
2022-11-11 13:34:41 -0500

    edited combiner app

--- src/main/scala/objektwerks/CombinerApp.scala
+++ src/main/scala/objektwerks/CombinerApp.scala
@@ -3,12 +3,33 @@ package objektwerks
 import zio.Console.{printLine, readLine}
 import zio.{Task, ZIO, ZIOAppDefault}
 
+/*
+NOTE: Boilerplate not required for app.
+      Only Combiner is required.
+
+package objektwerks
+
+import zio.{Task, URLayer, ZIO, ZLayer}
+
+trait Combiner:
+  def combine(a: String, b: String): Task[String]
+
+class CombinerLive extends Combiner:
+  override def combine(a: String, b: String): Task[String] = ZIO.attempt(s"$a$b")
+
+object Combiner:
+  def combine(a: String, b: String): ZIO[Combiner, Nothing, Task[String]] = ZIO.serviceWith[Combiner](_.combine(a, b))
+
+object CombinerLive:
+  val layer: URLayer[Any, Combiner] = ZLayer.succeed(CombinerLive())
+*/
+
 class Combiner:
   def combine(a: String, b: String): Task[String] = ZIO.attempt(s"$a$b")
 
 object CombinerApp extends ZIOAppDefault:
   def app(combiner: Combiner): Task[Unit] =
-        for {
+    for {
       _ <- printLine("Enter a string:")
       a <- readLine
       _ <- printLine("Enter another string:")
