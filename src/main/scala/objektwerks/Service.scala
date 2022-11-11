b22d86a3a0b8743562132faeed84583bb9e9ba7b
objektwerks <objektwerks@runbox.com>
2022-11-11 12:50:46 -0500

    edited service

--- src/main/scala/objektwerks/Service.scala
+++ src/main/scala/objektwerks/Service.scala
@@ -1,6 +1,8 @@
 package objektwerks
 
+import zio.{Task}
+
 trait Service:
-  def add(x: Int, y: Int): Int
-  def multiply(x: Int, y: Int): Int
+  def add(x: Int, y: Int): Task[Int]
+  def multiply(x: Int, y: Int): Task[Int]
 
