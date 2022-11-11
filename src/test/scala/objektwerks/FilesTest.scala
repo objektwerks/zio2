678f65a9f433d90a6071410ce96d48f92e882513
objektwerks <objektwerks@runbox.com>
2022-11-10 19:26:26 -0500

    edited files test

--- src/test/scala/objektwerks/FilesTest.scala
+++ src/test/scala/objektwerks/FilesTest.scala
@@ -3,10 +3,10 @@ package objektwerks
 import zio.test.{ assertTrue, ZIOSpecDefault }
 
 object FilesTest extends ZIOSpecDefault:
-  def spec = suite("file")(
+  def spec = suite("files")(
     test("read") {
       val file = Files.read("./LICENSE")
-      file.map(lines => assertTrue( lines.size == 48 ))
-      file.map(lines => assertTrue( lines.mkString(" ").length() == 9124 ))
+      file.map(lines => assertTrue(lines.size == 48))
+      file.map(lines => assertTrue(lines.mkString(" ").length() == 9124))
     }
   )
