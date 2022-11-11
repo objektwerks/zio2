02db61c917f30c56530a9e675a5da04eed7e2c26
objektwerks <objektwerks@runbox.com>
2022-11-11 13:28:23 -0500

    edited combiner

--- src/main/scala/objektwerks/Combiner.scala
+++ src/main/scala/objektwerks/Combiner.scala
@@ -5,11 +5,13 @@ import zio.{Task, URLayer, ZIO, ZLayer}
 trait Combiner:
   def combine(a: String, b: String): Task[String]
 
+class CombinerLive extends Combiner:
+  override def combine(a: String, b: String): Task[String] = ZIO.attempt(s"$a$b")
+
+// CombinerApp does not appear to require these objects.
+
 object Combiner:
   def combine(a: String, b: String): ZIO[Combiner, Nothing, Task[String]] = ZIO.serviceWith[Combiner](_.combine(a, b))
-
-class CombinerLive extends Combiner:
-  override def combine(a: String, b: String): Task[String] = ZIO.attempt(s"$a $b")
 
 object CombinerLive:
   val layer: URLayer[Any, Combiner] = ZLayer.succeed(CombinerLive())
