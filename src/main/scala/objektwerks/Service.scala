b5a094276cf4af71efc6298b7034c99c96af2852
objektwerks <objektwerks@runbox.com>
2022-11-11 13:00:05 -0500

    enhanced service

--- src/main/scala/objektwerks/Service.scala
+++ src/main/scala/objektwerks/Service.scala
@@ -1,8 +1,15 @@
 package objektwerks
 
-import zio.{Task}
+import zio.{Task, ZIO}
 
 trait Service:
   def add(x: Int, y: Int): Task[Int]
   def multiply(x: Int, y: Int): Task[Int]
 
+object Service:
+  def add(x: Int, y: Int): ZIO[Service, Nothing, Task[Int]] = ZIO.serviceWith[Service](_.add(x, y))
+  def multiply(x: Int, y: Int): ZIO[Service, Nothing, Task[Int]] = ZIO.serviceWith[Service](_.add(x, y))
+
+class ServiceLive extends Service:
+  override def add(x: Int, y: Int): Task[Int] = ZIO.attempt(x + y)
+  override def multiply(x: Int, y: Int): Task[Int] = ZIO.attempt(x * y)
