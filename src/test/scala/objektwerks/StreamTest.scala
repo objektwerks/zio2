8fcceb089620eae2c4f1187b428ee979a1bbde9d
objektwerks <objektwerks@runbox.com>
2022-11-12 16:53:39 -0500

    enhanced stream test

--- src/test/scala/objektwerks/StreamTest.scala
+++ src/test/scala/objektwerks/StreamTest.scala
@@ -7,6 +7,9 @@ import zio.test.{assertTrue, ZIOSpecDefault}
 
 object StreamTest extends ZIOSpecDefault:
   def spec = suite("streams")(
+    test("map") {
+      ZStream(1, 2, 3).map( i => i * 2).run(ZSink.sum).map( result => assertTrue( result == 12 ) )
+    },
     test("fold") {
       ZStream(1, 2, 3).runFold(0)(_ + _).map( result => assertTrue( result == 6 ) )
     },
