582299d0079a14047d052a60b96a81aa348e216e
objektwerks <objektwerks@runbox.com>
2022-11-12 14:46:14 -0500

    enhanced conf test

--- src/test/scala/objektwerks/ConfTest.scala
+++ src/test/scala/objektwerks/ConfTest.scala
@@ -1,7 +1,10 @@
 package objektwerks
 
+import com.typesafe.config.ConfigFactory
+
 import zio.config.*
-import zio.config.magnolia.Descriptor.*
+import zio.config.magnolia.descriptor
+import zio.config.typesafe.TypesafeConfigSource
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 case class Conf(string: String, integer: Int)
@@ -9,8 +12,12 @@ case class Conf(string: String, integer: Int)
 object ConfTest extends ZIOSpecDefault:
   def spec = suite("files")(
     test("conf") {
-      val conf = descriptor[Conf]
-      assertTrue(conf.string == "string")
-      assertTrue(conf.integer = 1)
+      for {
+        source <- TypesafeConfigSource.fromTypesafeConfig(ConfigFactory.load("./test.conf"))
+        conf   <- read( descriptor[Conf] from source )
+      } yield {
+        assertTrue(conf.string == "string")
+        assertTrue(conf.integer = 1)
+      }
     }
   )
