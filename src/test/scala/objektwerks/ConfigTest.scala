92b843e48401bc605d21e882b33b9296be53da5c
objektwerks <objektwerks@runbox.com>
2022-11-12 15:28:28 -0500

    moved test.conf

--- src/test/scala/objektwerks/ConfigTest.scala
+++ src/test/scala/objektwerks/ConfigTest.scala
@@ -4,8 +4,8 @@ import com.typesafe.config.ConfigFactory
 
 import zio.ZIO
 import zio.config.*
+import zio.config.typesafe.*
 import zio.config.magnolia.descriptor
-import zio.config.typesafe.TypesafeConfigSource
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 case class Conf(value: String, number: Int)
@@ -13,9 +13,13 @@ case class Conf(value: String, number: Int)
 object ConfigTest extends ZIOSpecDefault:
   def spec = suite("files")(
     test("conf") {
+      val config = ConfigFactory.load("test.conf")
+      println(config)
       val source = TypesafeConfigSource.fromTypesafeConfig( ZIO.attempt( ConfigFactory.load("test.conf") ) )
       val zconf = read( descriptor[Conf].from(source) )
-      zconf.map { conf => 
+      for {
+        conf <- zconf
+      } yield {
         assertTrue(conf.value == "value")
         assertTrue(conf.number == 1)
       }
