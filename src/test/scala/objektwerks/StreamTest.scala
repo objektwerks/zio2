fab5f691d123fa0e4b11ff4b96243c18d7b7bea5
objektwerks <objektwerks@runbox.com>
2022-11-13 17:42:52 -0500

    reformatted stream test

--- src/test/scala/objektwerks/StreamTest.scala
+++ src/test/scala/objektwerks/StreamTest.scala
@@ -9,32 +9,32 @@ object StreamTest extends ZIOSpecDefault:
   def spec = suite("streams")(
     test("map") {
       ZStream(1, 2, 3)
-      .map( i => i * 2 )
+      .map(i => i * 2)
       .run(ZSink.sum)
-      .map(result => assertTrue( result == 12 ))
+      .map(result => assertTrue(result == 12))
     },
     test("filter") {
       ZStream(1, 2, 3)
-      .filter( i => i % 2 == 0)
+      .filter(i => i % 2 == 0)
       .run(ZSink.sum)
-      .map(result => assertTrue( result == 2 ))
+      .map(result => assertTrue(result == 2))
     },
     test("fold") {
       ZStream(1, 2, 3)
       .runFold(0)(_ + _)
-      .map(result => assertTrue( result == 6 ))
+      .map(result => assertTrue(result == 6))
     },
     test("sum") {
       ZStream(1, 2, 3)
       .run(ZSink.sum)
-      .map(result => assertTrue( result == 6 ))
+      .map(result => assertTrue(result == 6))
     },
     test("chunk") {
       ZStream
-      .fromChunks( Chunk(1, 2, 3), Chunk(4, 5, 6), Chunk(7, 8, 9) )
-      .mapChunks( chunk => chunk.filter( i => i % 2 != 0) )
+      .fromChunks(Chunk(1, 2, 3), Chunk(4, 5, 6), Chunk(7, 8, 9))
+      .mapChunks(chunk => chunk.filter(i => i % 2 != 0))
       .run(ZSink.sum)
-      .map(result => assertTrue( result == 25 ))
+      .map(result => assertTrue(result == 25))
     },
     test("pipeline") {
       val strings = ZStream("1", "2", "3", "4", "5", "6")
@@ -46,16 +46,16 @@ object StreamTest extends ZIOSpecDefault:
       strings
       .via(pipeline)
       .run((ZSink.sum))
-      .map(result => assertTrue( result == 9 ))
+      .map(result => assertTrue(result == 9))
     },
     test("error") {
-      val invalidStream = ZStream(1, 2, 3) ++ ZStream.fail("Invalid Stream!") ++ ZStream(4, 5, 6)
+      val invalidStream = ZStream(1, 2, 3) ++ ZStream.fail("a", "b", "c") ++ ZStream(4, 5, 6)
       val validStream = ZStream(1, 2, 3, 4, 5, 6)
-      invalidStream  // ZStream(1, 2, 3) is included below!
+      invalidStream  // ZStream(1, 2, 3) is included below; hence the sum of 27!
       .orElse(validStream) // .catchAllCause(_ => validStream) produces same result.
       .tap(i => printLine(i))
       .run((ZSink.sum))
       .tap(i => printLine(i)) // ZStream(1, 2, 3) + ZStream(1, 2, 3, 4, 5, 6) = 27
-      .map(result => assertTrue( result == 27 ))
+      .map(result => assertTrue(result == 27))
     }
   )
