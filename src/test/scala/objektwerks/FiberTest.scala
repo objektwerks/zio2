2a7b4668521e769fcc08b3e7b5d19add7ef00631
objektwerks <objektwerks@runbox.com>
2022-11-13 07:22:31 -0500

    added fiber test

---
+++ src/test/scala/objektwerks/FiberTest.scala
@@ -0,0 +1,19 @@
+package objektwerks
+
+import zio.ZIO
+import zio.test.{assertTrue, ZIOSpecDefault}
+
+object FiberTest extends ZIOSpecDefault {
+  def spec = suite("fiber")(
+    test("fiber") {
+      for {
+        helloFiber <- ZIO.succeed("Hello, ").fork
+        worldFiber <- ZIO.succeed("world!").fork
+        tuple      <- (helloFiber zip worldFiber).join
+      } yield {
+        val (hello, world) = tuple
+        assertTrue(hello + world == "Hello, world!")
+      }
+    }
+  )
+}
