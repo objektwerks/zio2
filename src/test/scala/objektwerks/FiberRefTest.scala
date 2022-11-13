4a3c00443b42b9f6e96e7df5cdee55332980babd
objektwerks <objektwerks@runbox.com>
2022-11-13 17:32:24 -0500

    reformatted for comprehension

--- src/test/scala/objektwerks/FiberRefTest.scala
+++ src/test/scala/objektwerks/FiberRefTest.scala
@@ -6,12 +6,12 @@ import zio.test.{assertTrue, ZIOSpecDefault}
 object FiberRefTest extends ZIOSpecDefault {
   def spec = suite("fiber.ref")(
     test("fiber.ref") {
-      for {
+      for
         fiberRef <- FiberRef.make(1)
         initial  <- fiberRef.get
         _        <- fiberRef.set(initial * 2)
         result   <- fiberRef.get
-      } yield assertTrue(result == 2)
+      yield assertTrue(result == 2)
     }
   )
 }
