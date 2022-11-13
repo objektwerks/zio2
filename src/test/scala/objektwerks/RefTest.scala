e23778382981b80fca49e8a5cdd76cdbc1e3198b
objektwerks <objektwerks@runbox.com>
2022-11-13 07:11:40 -0500

    added ref test

---
+++ src/test/scala/objektwerks/RefTest.scala
@@ -0,0 +1,16 @@
+package objektwerks
+
+import zio.Ref
+import zio.test.{assertTrue, ZIOSpecDefault}
+
+object RefTest extends ZIOSpecDefault:
+  def spec = suite("ref")(
+    test("ref") {
+      for {
+        ref     <- Ref.make(1)
+        initial <- ref.get
+        _       <- ref.set(initial * 2)
+        result  <- ref.get
+      } yield assertTrue(result == 2)
+    }
+  )
