24450cb032932721d953e119ab300e18860e2cca
objektwerks <objektwerks@runbox.com>
2022-11-14 18:24:09 -0500

    enhanced logger

--- src/main/scala/objektwerks/Logger.scala
+++ src/main/scala/objektwerks/Logger.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import zio.{Console, UIO, URIO, ZIO, ZLayer}
+import zio.{Console, Scope, Task, UIO, URIO, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 // 1. Official ZIO Service Pattern
 
@@ -29,3 +29,34 @@ class Log:
 // Service ZLayer, No Accessor Method
 object Log:
   val layer: ZLayer[Any, Nothing, Log] = ZLayer.succeed(Log())
+
+// 3. Root Service
+
+// Service Implementor, No Trait
+class LogHolder(logger: Logger, log: Log):
+  def logAll: ZIO[Any, Nothing, Unit] =
+    for {
+      _ <- logger.log("*** LogHolder -> Logger log message!")
+      _ <- log.log("*** LogHolder -> Log log message!")
+    } yield ()
+
+// Service ZLayer, No Accessor Method
+object LogHolder:
+  val layer: ZLayer[Logger with Log, Nothing, LogHolder] =
+    ZLayer {
+      for
+        logger <- ZIO.service[Logger]
+        log    <- ZIO.service[Log]
+      yield LogHolder(logger, log)
+    }
+
+object LoggerLogApp extends ZIOAppDefault:
+  def app(logHolder: LogHolder): ZIO[Any, Throwable, Unit] =
+    for
+      _ <- logHolder.logAll
+    yield()
+
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] =
+    ZIO
+      .serviceWithZIO[LogHolder](app)
+      .provide(LogHolder.layer, LoggerLive.layer, Log.layer)
