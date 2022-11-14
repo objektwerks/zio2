74e151b0d14d762a790c5a5a8ff58eb7b17d9e24
objektwerks <objektwerks@runbox.com>
2022-11-14 14:00:20 -0500

    enhanced errors test

--- src/test/scala/objektwerks/ErrorsTest.scala
+++ src/test/scala/objektwerks/ErrorsTest.scala
@@ -1,6 +1,7 @@
 package objektwerks
 
 import zio.test.{assertTrue, ZIOSpecDefault}
+import java.io.IOException
 
 object ErrorsTest extends ZIOSpecDefault:
   def spec = suite("errors")(
@@ -8,6 +9,13 @@ object ErrorsTest extends ZIOSpecDefault:
       Files.read("./LIC").catchAll( _ =>
         Files.read("./LICENSE")
       ).map { lines =>
+        assertTrue(lines.size == 48)
+      }
+    },
+    test("catchSome") {
+      Files.read("./LIC").catchSome { case ioe: IOException =>
+        Files.read("./LICENSE")
+      }.map { lines =>
         assertTrue(lines.size == 48)
       }
     }
