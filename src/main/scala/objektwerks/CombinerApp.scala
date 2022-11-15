66dfb5b9b9c1191bab8322cb974936db6ae3f9bf
objektwerks <objektwerks@runbox.com>
2022-11-15 07:00:23 -0500

    added comments

--- src/main/scala/objektwerks/CombinerApp.scala
+++ src/main/scala/objektwerks/CombinerApp.scala
@@ -3,9 +3,11 @@ package objektwerks
 import zio.Console.{printLine, readLine}
 import zio.{Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
+// Service Implementor, No Trait
 class Combiner:
   def combine(a: String, b: String): Task[String] = ZIO.attempt(a + b)
 
+// Service ZLayer, No Accessor Method
 object Combiner:
   val layer: ZLayer[Any, Nothing, Combiner] = ZLayer.succeed(Combiner())
 
