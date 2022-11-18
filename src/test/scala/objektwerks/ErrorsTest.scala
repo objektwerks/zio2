8a600d18bd0f55923503054e75e64296a07924c0
objektwerks <objektwerks@runbox.com>
2022-11-18 10:05:23 -0500

    refactored errors test

--- src/test/scala/objektwerks/ErrorsTest.scala
+++ src/test/scala/objektwerks/ErrorsTest.scala
@@ -10,19 +10,19 @@ object ErrorsTest extends ZIOSpecDefault:
     test("catchAll") {
       Files
         .read("./LIC")
-        .catchAll( _ =>
-          Files.read("./LICENSE")
-        ).map { lines =>
-          assertTrue(lines.size == 48)
-        }
+        .catchAll( _ => Files.read("./LICENSE") )
+        .map(lines => assertTrue(lines.size == 48))
     },
     test("catchSome") {
       Files
         .read("./LIC")
-        .catchSome { case ioe: IOException =>
-          Files.read("./LICENSE")
-        }.map { lines =>
-          assertTrue(lines.size == 48)
-        }
+        .catchSome { case ioe: IOException => Files.read("./LICENSE") }
+        .map(lines => assertTrue(lines.size == 48))
+    },
+    test("orElse") {
+      Files
+        .read("./LIC")
+        .orElse( Files.read("./LICENSE") )
+        .map(lines => assertTrue(lines.size == 48))
     }
   )
