faea367ac3c4b6ca2ba485a8c8d897fbd520703c
objektwerks <objektwerks@runbox.com>
2022-11-18 09:37:17 -0500

    reformatted errors test

--- src/test/scala/objektwerks/ErrorsTest.scala
+++ src/test/scala/objektwerks/ErrorsTest.scala
@@ -6,17 +6,21 @@ import java.io.IOException
 object ErrorsTest extends ZIOSpecDefault:
   def spec = suite("errors")(
     test("catchAll") {
-      Files.read("./LIC").catchAll( _ =>
-        Files.read("./LICENSE")
-      ).map { lines =>
-        assertTrue(lines.size == 48)
-      }
+      Files
+        .read("./LIC")
+        .catchAll( _ =>
+          Files.read("./LICENSE")
+        ).map { lines =>
+          assertTrue(lines.size == 48)
+        }
     },
     test("catchSome") {
-      Files.read("./LIC").catchSome { case ioe: IOException =>
-        Files.read("./LICENSE")
-      }.map { lines =>
-        assertTrue(lines.size == 48)
-      }
+      Files
+        .read("./LIC")
+        .catchSome { case ioe: IOException =>
+          Files.read("./LICENSE")
+        }.map { lines =>
+          assertTrue(lines.size == 48)
+        }
     }
   )
