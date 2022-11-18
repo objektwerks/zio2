74ae42c96ca6f306f0aa095be42af4b3d219e264
objektwerks <objektwerks@runbox.com>
2022-11-18 08:37:57 -0500

    enhanced effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -5,10 +5,16 @@ import zio.test.{assertTrue, ZIOSpecDefault}
 
 object EffectTest extends ZIOSpecDefault:
   def spec = suite("effect")(
-    test("effects") {
+    test("map") {
       ZIO
         .succeed(1)
         .map(value => value + 1)
+        .map(result => assertTrue(result == 2))
+    },
+    test("flatMap") {
+      ZIO
+        .succeed(1)
+        .flatMap(value => ZIO.succeed(value + 1))
         .map(result => assertTrue(result == 2))
     }
   )
