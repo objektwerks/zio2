41bbdaac7252644c9d78d1bf2ea0b6a005558663
objektwerks <objektwerks@runbox.com>
2022-11-19 13:25:54 -0500

    edited subscription app

--- src/main/scala/objektwerks/SubscriptionApp.scala
+++ src/main/scala/objektwerks/SubscriptionApp.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import zio.{Runtime, Task, ZIO, ZIOAppDefault, ZLayer}
+import zio.{Runtime, Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 case class User(name: String, email: String)
 
@@ -46,4 +46,4 @@ object SubscriptionApp extends ZIOAppDefault:
       _       <- service.subscribe( User("Barney Rubble", "barney.rubble@rock.com") )
     yield ()
 
-  override def run = app.provide(SubscriptionService.layer)
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(SubscriptionService.layer)
