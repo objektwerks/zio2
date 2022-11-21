926d7f9901e93e9dc396c343e9e3d87872b6f929
objektwerks <objektwerks@runbox.com>
2022-11-21 12:06:08 -0500

    added resource

--- src/test/scala/objektwerks/ResourceTest.scala
+++ src/test/scala/objektwerks/ResourceTest.scala
@@ -3,13 +3,6 @@ package objektwerks
 import zio.ZIO
 import zio.test.{assertTrue, ZIOSpecDefault}
 
-case class Resource(url: String):
-  def open: ZIO[Any, Nothing, String] = ZIO.succeed(s"opening resource: $url").debug
-  def close: ZIO[Any, Nothing, String] = ZIO.succeed(s"closing resource: $url").debug
-
-object Resource:
-  def apply(url: String): ZIO[Any, Nothing, Resource] = ZIO.succeed(new Resource(url))
-
 object ResourceTest extends ZIOSpecDefault:
   def spec = suite("resource")(
     test("file") {
