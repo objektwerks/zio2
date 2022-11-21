aa3503b7e156771949f23ac8454b09461adbabad
objektwerks <objektwerks@runbox.com>
2022-11-21 06:59:04 -0500

    edited errors test

--- src/test/scala/objektwerks/ErrorsTest.scala
+++ src/test/scala/objektwerks/ErrorsTest.scala
@@ -7,7 +7,6 @@ import scala.util.Try
 import zio.ZIO
 import zio.test.{assertTrue, ZIOSpecDefault}
 
-
 object ErrorsTest extends ZIOSpecDefault:
   def spec = suite("errors")(
     test("catchAll") {
