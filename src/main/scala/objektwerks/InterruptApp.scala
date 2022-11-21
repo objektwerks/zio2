151c5cc3730d64527e2c0d18695f8dbd4562a05c
objektwerks <objektwerks@runbox.com>
2022-11-21 07:29:19 -0500

    refactored interrupt app

--- src/main/scala/objektwerks/InterruptApp.scala
+++ src/main/scala/objektwerks/InterruptApp.scala
@@ -3,10 +3,15 @@ package objektwerks
 import zio.{durationInt, Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 object InterruptApp extends ZIOAppDefault:
+  val effect = (
+    ZIO.succeed("fiber running...").debug
+    *> ZIO.sleep(1.second)
+    *> ZIO.succeed("fiber completed!").debug
+  ).onInterrupt(ZIO.succeed("fiber interrupted!")).debug
+
   val app =
     for
-      f <- ZIO.succeed("fiber").fork
-      _ <- ZIO.sleep(1.second)
+      f <- effect.fork
       _ <- ZIO.succeed("interrupting fiber...").debug
       _ <- f.interruptFork
       _ <- ZIO.succeed("interrupted fiber!").debug
