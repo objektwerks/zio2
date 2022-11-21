05d369d4e46a8cc22a95e8c30637e32ff5abc9d5
objektwerks <objektwerks@runbox.com>
2022-11-21 07:18:22 -0500

    added fiber app

---
+++ src/main/scala/objektwerks/FiberApp.scala
@@ -0,0 +1,14 @@
+package objektwerks
+
+import zio.{durationInt, Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+
+object FiberApp extends ZIOAppDefault:
+  val app =
+    for
+      f <- ZIO.succeed("fiber").fork
+      _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting fiber...").debug *> f.interruptFork
+      _ <- ZIO.succeed("interrupted fiber!").debug
+      r <- f.join
+    yield r
+
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
