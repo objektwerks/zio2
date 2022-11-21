ec968acef4c3b04c92d46476ae8cfe2f346b3fa9
objektwerks <objektwerks@runbox.com>
2022-11-21 11:42:16 -0500

    enhanced resource test

--- src/test/scala/objektwerks/ResourceTest.scala
+++ src/test/scala/objektwerks/ResourceTest.scala
@@ -4,8 +4,8 @@ import zio.ZIO
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 case class Resource(url: String):
-  def open(): ZIO[Any, Nothing, String] = ZIO.succeed(s"opening resource: $url").debug
-  def close(): ZIO[Any, Nothing, String] = ZIO.succeed(s"closing resource: $url").debug
+  def open: ZIO[Any, Nothing, String] = ZIO.succeed(s"opening resource: $url").debug
+  def close: ZIO[Any, Nothing, String] = ZIO.succeed(s"closing resource: $url").debug
 
 object Resource:
   def apply(url: String): ZIO[Any, Nothing, Resource] = ZIO.succeed(new Resource(url))
@@ -16,5 +16,12 @@ object ResourceTest extends ZIOSpecDefault:
       Files.read("./LICENSE").map { lines =>
         assertTrue(lines.size == 48) && assertTrue(lines.mkString(" ").length() == 9124)
       }
+    },
+    test("resource") {
+      for
+        resource <- Resource("objektwerks.com")
+        open     <- resource.open
+        close    <- resource.close
+      yield assertTrue(open.contains("opening")) && assertTrue(close.contains("closing"))
     }
   )
