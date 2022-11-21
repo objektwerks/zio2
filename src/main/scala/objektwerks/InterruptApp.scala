6efc4b0afa7eefe8347ae52ab7eb4e865331f040
objektwerks <objektwerks@runbox.com>
2022-11-21 07:20:29 -0500

    edited interrupt app

--- src/main/scala/objektwerks/InterruptApp.scala
+++ src/main/scala/objektwerks/InterruptApp.scala
@@ -6,7 +6,9 @@ object InterruptApp extends ZIOAppDefault:
   val app =
     for
       f <- ZIO.succeed("fiber").fork
-      _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting fiber...").debug *> f.interruptFork
+      _ <- ZIO.sleep(1.second)
+      _ <- ZIO.succeed("interrupting fiber...").debug
+      _ <- f.interruptFork
       _ <- ZIO.succeed("interrupted fiber!").debug
       r <- f.join
     yield r
