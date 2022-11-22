2e0d46b65aac3bb11a5a8b40882f9cc108603ece
objektwerks <objektwerks@runbox.com>
2022-11-22 12:25:00 -0500

    refactored imports

--- src/test/scala/objektwerks/DependencyTest.scala
+++ src/test/scala/objektwerks/DependencyTest.scala
@@ -1,9 +1,9 @@
 package objektwerks
 
-import zio.{durationInt, Console, ZIO, ZLayer}
+import zio.{durationInt, Console, ZIO}
 import zio.test.{assertZIO, TestConsole, ZIOSpecDefault}
-import zio.test.Assertion.*
-import zio.test.TestAspect.*
+import zio.test.Assertion.containsString
+import zio.test.TestAspect.timeout
 
 object DependencyTest extends ZIOSpecDefault:
   def spec = suite("dependency")(
