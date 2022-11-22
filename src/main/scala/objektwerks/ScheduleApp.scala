69b6c2c44b198bdf7e94e9d20d01adec2b6aeb81
objektwerks <objektwerks@runbox.com>
2022-11-22 08:02:52 -0500

    added types to schedule app for clarity

--- src/main/scala/objektwerks/ScheduleApp.scala
+++ src/main/scala/objektwerks/ScheduleApp.scala
@@ -3,11 +3,11 @@ package objektwerks
 import zio.{Random, Schedule, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object ScheduleApp extends ZIOAppDefault:
-  val effect = Random.nextBoolean.flatMap { isTrue =>
+  val effect: ZIO[Any, String, String] = Random.nextBoolean.flatMap { isTrue =>
     if isTrue then ZIO.succeed("random boolean is true").debug
     else ZIO.succeed("random boolean is false").debug *> ZIO.fail("random effect failed")
   }
 
-  val app = effect.retry(Schedule.recurs(5))
+  val app: ZIO[Any, String, String] = effect.retry(Schedule.recurs(5))
     
   override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
