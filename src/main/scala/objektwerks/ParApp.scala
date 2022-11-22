8ba048319f12886cc58fb509e2d8155f8a3e037f
objektwerks <objektwerks@runbox.com>
2022-11-22 06:42:32 -0500

    added types for clarity

--- src/main/scala/objektwerks/ParApp.scala
+++ src/main/scala/objektwerks/ParApp.scala
@@ -4,9 +4,9 @@ import zio.{durationInt, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object ParApp extends ZIOAppDefault:
   val zios: Seq[ZIO[Any, Nothing, Int]] = (1 to 1_000_000).map(i => ZIO.succeed(i))
-  val effect = ZIO.reduceAllPar(ZIO.succeed(0), zios)(_ + _)
+  val effect: ZIO[Any, Nothing, Int] = ZIO.reduceAllPar(ZIO.succeed(0), zios)(_ + _)
 
-  val app =
+  val app: ZIO[Any, Nothing, Unit] =
     for
       fiber  <- effect.fork
       result <- fiber.join
