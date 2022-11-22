8d6d784ca836d256ea2e9bbe52959d425e4abfa7
objektwerks <objektwerks@runbox.com>
2022-11-22 08:54:41 -0500

    reformatted effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -7,15 +7,23 @@ import zio.test.Assertion.*
 object EffectTest extends ZIOSpecDefault:
   def spec = suite("effect")(
     test("map") {
-      assertZIO(ZIO.succeed(1).map(value => value + 1))(equalTo(2))
+      assertZIO(
+        ZIO.succeed(1).map(value => value + 1)
+      )(equalTo(2))
     },
     test("flatMap") {
-      assertZIO(ZIO.succeed(1).flatMap(value => ZIO.succeed(value + 1)))(equalTo(2))
+      assertZIO(
+        ZIO.succeed(1).flatMap(value => ZIO.succeed(value + 1))
+      )(equalTo(2))
     },
     test("zip") {
-      assertZIO(ZIO.succeed(1).zip(ZIO.succeed(1)).map(tuple => tuple._1 + tuple._2))(equalTo(2))
+      assertZIO(
+        ZIO.succeed(1).zip(ZIO.succeed(1)).map(tuple => tuple._1 + tuple._2)
+      )(equalTo(2))
     },
     test("zipWith") {
-      assertZIO(ZIO.succeed(1).zipWith(ZIO.succeed(1))(_ + _))(equalTo(2))
+      assertZIO(
+        ZIO.succeed(1).zipWith(ZIO.succeed(1))(_ + _)
+      )(equalTo(2))
     }
   )
