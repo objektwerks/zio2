b6f0c221d10308721c2950c541ebfe4d32f7becd
objektwerks <objektwerks@runbox.com>
2022-11-22 12:45:43 -0500

    refactored property based test

--- src/test/scala/objektwerks/PropertyBasedTest.scala
+++ src/test/scala/objektwerks/PropertyBasedTest.scala
@@ -1,10 +1,14 @@
 package objektwerks
 
-import zio.test.{assertTrue, ZIOSpecDefault}
+import zio.test.{assertTrue, check, Gen, ZIOSpecDefault}
 
 object PropertyBasedTest extends ZIOSpecDefault:
+  def add(x: Int, y: Int): Int = x + y
+
   def spec = suite("property based")(
     test("property") {
-      assertTrue(true)
+      check(Gen.int, Gen.int) { (x, y) =>
+        assertTrue(add(x, y) == add(y, x))
+      }
     }
   )
