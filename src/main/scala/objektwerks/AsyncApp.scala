8e30d58129436757d35a361f76e70034abddc761
objektwerks <objektwerks@runbox.com>
2022-11-22 07:58:04 -0500

    added type to asyn app for clarity

--- src/main/scala/objektwerks/AsyncApp.scala
+++ src/main/scala/objektwerks/AsyncApp.scala
@@ -6,13 +6,13 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import zio.{durationInt, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object AsyncApp extends ZIOAppDefault:
-  val effect = ZIO.fromFuture( Future(1 + 1) )
+  val effect: ZIO[Any, Throwable, Int] = ZIO.fromFuture( Future(1 + 1) )
 
   val app: ZIO[Any, Throwable, Int] =
     for
       _      <- ZIO.succeed("Future calculating...").debug
-      result  <- effect
-       _      <- ZIO.succeed(s"Future calculated: $result").debug
+      result <- effect
+       _     <- ZIO.succeed(s"Future calculated: $result").debug
     yield result
     
   override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
