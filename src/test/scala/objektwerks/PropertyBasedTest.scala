e09c34f66a026c1fcf5895c4598f7a0eae0e3844
objektwerks <objektwerks@runbox.com>
2022-11-22 12:41:34 -0500

    added property based test

---
+++ src/test/scala/objektwerks/PropertyBasedTest.scala
@@ -0,0 +1,10 @@
+package objektwerks
+
+import zio.test.{assertTrue, ZIOSpecDefault}
+
+object PropertyBasedTest extends ZIOSpecDefault:
+  def spec = suite("property based")(
+    test("property") {
+
+    }
+  )
