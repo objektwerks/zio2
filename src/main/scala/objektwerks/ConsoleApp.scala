2179900dd20e29835becfed6a96565d06becc9b7
objektwerks <objektwerks@runbox.com>
2022-11-23 12:03:04 -0500

    added zio logging to console app

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -1,12 +1,17 @@
 package objektwerks
 
 import zio.Console.{printLine, readLine}
-import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.logging.{LogFormat, console}
 
 object ConsoleApp extends ZIOAppDefault:
+  override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] = Runtime.removeDefaultLoggers >>> console(LogFormat.default)
+
   def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
     for
+      _    <- ZIO.logInfo("Greetings! What is your name?")
       _    <- printLine("*** Greetings! What is your name?")
       name <- readLine
+      _    <- ZIO.logInfo(s"Welcome, ${name}!")
       _    <- printLine(s"*** Welcome, ${name}!")
     yield ()
