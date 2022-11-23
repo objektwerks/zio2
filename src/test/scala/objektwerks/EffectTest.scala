4c3959225dd021bb37a1e335a3832c8b791f8c6d
objektwerks <objektwerks@runbox.com>
2022-11-23 12:22:39 -0500

    enhanced effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -3,6 +3,7 @@ package objektwerks
 import zio.ZIO
 import zio.test.{assertTrue, assertZIO, ZIOSpecDefault}
 import zio.test.Assertion.*
+import zio.ExitCode
 
 object EffectTest extends ZIOSpecDefault:
   def spec = suite("effect")(
@@ -25,5 +26,10 @@ object EffectTest extends ZIOSpecDefault:
       assertZIO(
         ZIO.succeed(1).zipWith(ZIO.succeed(1))(_ + _)
       )(equalTo(2))
-    }
+    },
+    test("exit") {
+      assertZIO(
+        ZIO.succeed(1).map(value => value + 1).exit.map(exit => exit.isSuccess)
+      )(isTrue)
+    },
   )
