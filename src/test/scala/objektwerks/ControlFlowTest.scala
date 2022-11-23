45777a34eaefa685e84ad08f734102b8bce5b075
objektwerks <objektwerks@runbox.com>
2022-11-23 10:17:43 -0500

    enhanced control flow test

--- src/test/scala/objektwerks/ControlFlowTest.scala
+++ src/test/scala/objektwerks/ControlFlowTest.scala
@@ -8,16 +8,21 @@ import zio.test.{assertZIO, ZIOSpecDefault}
 import zio.test.Assertion.*
 
 object ControlFlowTest extends ZIOSpecDefault:
-  def isGreaterThanZero(value: Int): ZIO[Any, String, Int] =
-    if (value > 0)
-      ZIO.succeed(value)
-    else
-      ZIO.fail(s"Less than or equal to zero: $value")
+  def ifGreaterThanZero(value: Int): ZIO[Any, String, Int] =
+    if (value > 0) ZIO.succeed(value) else ZIO.fail(s"Less than or equal to zero: $value")
+
+  def whenGreaterThanZero(value: Int): ZIO[Any, Nothing, Option[Int]] =
+    ZIO.when(value > 0)(ZIO.succeed(value))
 
   def spec = suite("control flow")(
     test("if") {
       assertZIO(
-        isGreaterThanZero(1)
+        ifGreaterThanZero(1)
       )(equalTo(1))
+    },
+    test("when") {
+      assertZIO(
+        whenGreaterThanZero(1)
+      )(isSome)
     }
   )
