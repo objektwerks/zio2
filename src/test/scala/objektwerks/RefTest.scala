f9c6bc9d8c0d1761cfd98cecb54c8c572c0c8059
objektwerks <objektwerks@runbox.com>
2022-11-23 07:54:16 -0500

    enhanced ref test

--- src/test/scala/objektwerks/RefTest.scala
+++ src/test/scala/objektwerks/RefTest.scala
@@ -5,11 +5,18 @@ import zio.test.{assertTrue, ZIOSpecDefault}
 
 object RefTest extends ZIOSpecDefault:
   def spec = suite("ref")(
-    test("ref") {
+    test("get > set") {
       for
         ref     <- Ref.make(1)
         initial <- ref.get
         _       <- ref.set(initial + 1)
+        result  <- ref.get
+      yield assertTrue(result == 2)
+    },
+    test("getAndSet") {
+      for
+        ref     <- Ref.make(1)
+        _       <- ref.getAndSet(2)
         result  <- ref.get
       yield assertTrue(result == 2)
     }
