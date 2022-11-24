9a97cbee9fd913a77ea8b14e51c5d4ebc7eae334
objektwerks <objektwerks@runbox.com>
2022-11-24 17:33:08 -0500

    refactored combiner app

--- src/main/scala/objektwerks/CombinerApp.scala
+++ src/main/scala/objektwerks/CombinerApp.scala
@@ -4,12 +4,12 @@ import zio.Console.{printLine, readLine}
 import zio.{Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 // Service Implementor, No Trait
-class Combiner:
+case class Combiner():
   def combine(a: String, b: String): Task[String] = ZIO.attempt(a + b)
 
 // Service ZLayer, No Accessor Method
 object Combiner:
-  val layer: ZLayer[Any, Nothing, Combiner] = ZLayer.succeed(Combiner())
+  val layer: ZLayer[Any, Nothing, Combiner] = ZLayer.succeed(apply())
 
 object CombinerApp extends ZIOAppDefault:
   def app(combiner: Combiner): ZIO[Any, Throwable, Unit] =
