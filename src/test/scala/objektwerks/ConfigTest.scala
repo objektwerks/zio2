bd344d8b8222acd534ea6a0f368daf8a4f5eb432
objektwerks <objektwerks@runbox.com>
2022-11-24 18:56:19 -0500

    enhanced config test

--- src/test/scala/objektwerks/ConfigTest.scala
+++ src/test/scala/objektwerks/ConfigTest.scala
@@ -11,8 +11,7 @@ case class Conf(value: String, number: Int)
 object ConfigTest extends ZIOSpecDefault:
   def spec = suite("config")(
     test("conf") {
-      val source = TypesafeConfigSource.fromTypesafeConfig( Resources.loadConfig("test.conf", "Conf") )
-      read( descriptor[Conf].from(source) ).map { conf =>
+      read( descriptor[Conf].from( Resources.loadConfigSource("test.conf", "Conf") ) ).map { conf =>
         assertTrue(conf.value == "value") && assertTrue(conf.number == 1)
       }
     }
