13665dc3ded370e69c0198655d9d6251ea57700e
objektwerks <objektwerks@runbox.com>
2022-11-24 18:28:54 -0500

    refactored config test

--- src/test/scala/objektwerks/ConfigTest.scala
+++ src/test/scala/objektwerks/ConfigTest.scala
@@ -2,6 +2,10 @@ package objektwerks
 
 import com.typesafe.config.ConfigFactory
 
+import com.typesafe.config.Config
+
+import java.io.IOException
+
 import zio.ZIO
 import zio.config.read
 import zio.config.magnolia.descriptor
@@ -11,10 +15,17 @@ import zio.test.{assertTrue, ZIOSpecDefault}
 case class Conf(value: String, number: Int)
 
 object ConfigTest extends ZIOSpecDefault:
+  def loadConfig(path: String, section: String): ZIO[Any, IOException, Config] =
+    ZIO.attemptBlockingIO(
+      ConfigFactory
+        .load(path)
+        .getObject(section)
+        .toConfig
+    )
+
   def spec = suite("config")(
     test("conf") {
-      val config = ConfigFactory.load("test.conf").getObject("Conf").toConfig // bug!!!
-      val source = TypesafeConfigSource.fromTypesafeConfig( ZIO.attempt(config) )
+      val source = TypesafeConfigSource.fromTypesafeConfig( loadConfig("test.conf", "Conf") )
       read( descriptor[Conf].from(source) ).map { conf =>
         assertTrue(conf.value == "value") && assertTrue(conf.number == 1)
       }
