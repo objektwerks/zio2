8e46f802fde72de2ad6c38e7b1c1addd69ef6c43
objektwerks <objektwerks@runbox.com>
2022-11-25 04:07:17 -0500

    refactored validate app

--- src/main/scala/objektwerks/ValidateApp.scala
+++ src/main/scala/objektwerks/ValidateApp.scala
@@ -3,8 +3,8 @@ package objektwerks
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object ValidateApp extends ZIOAppDefault:
-  def app(values: List[Int], limit: Int): ZIO[Any, List[String], List[Int]] =
-    ZIO.validate(values) { i =>
+  def app(values: List[Int], limit: Int): ZIO[Any, Nothing, (Iterable[String], Iterable[Int])] =
+    ZIO.partition(values) { i =>
       if (i < limit) then ZIO.succeed(i)
       else ZIO.fail(s"$i >= $limit")
     }
