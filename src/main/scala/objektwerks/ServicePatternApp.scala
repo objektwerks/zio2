f11189cd1081aa0fdca9eaf0489cfbf63295d98f
objektwerks <objektwerks@runbox.com>
2022-11-27 08:23:08 -0500

    enhanced service pattern app

--- src/main/scala/objektwerks/ServicePatternApp.scala
+++ src/main/scala/objektwerks/ServicePatternApp.scala
@@ -31,25 +31,27 @@ final case class CDefault(a: A, b: B) extends C:
       c <- c
     yield a + b + c
 
-import zio._
-
-// Service Layers
-object ADefault:
+// Service Accessors
+object A:
   def a: ZIO[A, Nothing, String] = ZIO.serviceWithZIO[A](_.a)
 
-  val layer: ZLayer[Any, Nothing, A] = ZLayer.succeed(apply())
-
-object BDefault:
+object B:
   def b: ZIO[B, Nothing, String] = ZIO.serviceWithZIO[B](_.b)
 
-  val layer: ZLayer[Any, Nothing, B] = ZLayer.succeed(apply())
-
-object CDefault:
+object C:
   def c: ZIO[C, Nothing, String] = ZIO.serviceWithZIO[C](_.c)
 
   def abc: ZIO[C, Nothing, String] = ZIO.serviceWithZIO[C](_.abc)
 
-  val layer: ZLayer[B with A, Nothing, C] =
+// Service Layers
+object ADefault:
+  val layer: ZLayer[Any, Nothing, A] = ZLayer.succeed(apply())
+
+object BDefault:
+  val layer: ZLayer[Any, Nothing, B] = ZLayer.succeed(apply())
+
+object CDefault:
+  val layer: ZLayer[A with B, Nothing, C] =
     ZLayer {
       for {
         a <- ZIO.service[A]
@@ -58,8 +60,8 @@ object CDefault:
     }
 
 object ServicePatternApp extends ZIOAppDefault:
-  val app: ZIO[A with B with C, Nothing, Boolean] =
-    for {
+  def app: ZIO[A & B & C, Nothing, Boolean] =
+    for
       a <- ZIO.service[A]
       b <- ZIO.service[B]
       c <- ZIO.service[C]
@@ -67,10 +69,20 @@ object ServicePatternApp extends ZIOAppDefault:
       bs <- b.b.debug
       cs <- c.c.debug
       abc <- c.abc.debug
-    } yield (as + bs + cs) == abc
+    yield (as + bs + cs) == abc
 
-  val appLayer: ZLayer[Any, Nothing, A with B with C] =
-    ZLayer.make[A with B with C](ADefault.layer, BDefault.layer, CDefault.layer)
+  def appx: ZIO[A & B & C, Nothing, Boolean] =
+    for
+      as <- A.a.debug
+      bs <- B.b.debug
+      cs <- C.c.debug
+      abc <- C.abc.debug
+    yield (as + bs + cs) == abc
+
+  def appLayer: ZLayer[Any, Nothing, A & B & C] =
+    ZLayer.make[A & B & C](ADefault.layer, BDefault.layer, CDefault.layer)
 
 
-  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(appLayer)
+  // override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(appLayer)
+
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = appx.provide(appLayer)
