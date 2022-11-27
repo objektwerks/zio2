21c7f6448b672cd8232d5acdebd9318c44f64824
objektwerks <objektwerks@runbox.com>
2022-11-27 18:02:44 -0500

    added assert to system app

--- src/main/scala/objektwerks/SystemApp.scala
+++ src/main/scala/objektwerks/SystemApp.scala
@@ -3,6 +3,9 @@ package objektwerks
 import zio.{Console, Runtime, Scope, System, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 import zio.logging.console
 
+/**
+  * System.env("JAVA_HOME") fails in a ZIO test. Why?
+  */
 object SystemApp extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> console()
@@ -13,6 +16,6 @@ object SystemApp extends ZIOAppDefault:
       jh  =  opt.getOrElse("System env failed!")
       _   <- ZIO.log(s"JAVA_HOME = $jh")
       _   <- Console.printLine(s"JAVA_HOME = $jh")
-    yield ()
+    yield assert(jh != "System env failed!")
 
   def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
