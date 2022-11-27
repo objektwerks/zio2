18f73b16b79ca7abe869b701be81a714fc278545
objektwerks <objektwerks@runbox.com>
2022-11-27 07:43:47 -0500

    enhanced service pattern app

--- src/main/scala/objektwerks/ServicePatternApp.scala
+++ src/main/scala/objektwerks/ServicePatternApp.scala
@@ -6,11 +6,28 @@ import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 trait A:
   def a: ZIO[Any, Nothing, String]
+
 trait B:
   def b: ZIO[Any, Nothing, String]
+
 trait C:
   def c: ZIO[Any, Nothing, String]
   def abc: ZIO[Any, Nothing, String]
+
+final case class ADefault() extends A:
+  def a = ZIO.succeed("a")
+
+final case class BDefault() extends B:
+  def b: ZIO[Any, Nothing, String] = ZIO.succeed("b")
+
+final case class CDefault(a: A, b: B) extends C:
+  def c: ZIO[Any, Nothing, String] = ZIO.succeed("c")
+  def abc: ZIO[Any, Nothing, String] =
+    for
+      a <- a.a
+      b <- b.b
+      c <- c
+    yield a + b + c
 
 object ServicePatternApp extends ZIOAppDefault:
   override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = ???
