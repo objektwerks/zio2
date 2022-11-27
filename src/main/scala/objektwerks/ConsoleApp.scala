4198c9768c2725fd77130b05eb8aff1d6a7c939f
objektwerks <objektwerks@runbox.com>
2022-11-27 17:33:48 -0500

    refactored console app

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -1,16 +1,17 @@
 package objektwerks
 
+import java.io.IOException
 import java.nio.file.Path
 
 import zio.Console.{printLine, readLine}
-import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.{Runtime, Scope, System, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 import zio.logging.{LogFormat, file}
 
 object ConsoleApp extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> file(Path.of("./target/console.log"))
 
-  def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
+  def app: ZIO[Any, IOException, Unit] =
     for
       _    <- ZIO.log("Greetings! What is your name?")
       _    <- printLine("*** Greetings! What is your name?")
@@ -18,3 +19,5 @@ object ConsoleApp extends ZIOAppDefault:
       _    <- ZIO.log(s"Welcome, ${name}!")
       _    <- printLine(s"*** Welcome, ${name}!")
     yield ()
+
+  def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] = app
