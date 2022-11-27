e54af5483e9f8519e593f9c3697a03d05699116c
objektwerks <objektwerks@runbox.com>
2022-11-27 12:01:00 -0500

    enhanced config native test

--- src/test/scala/objektwerks/ConfigNativeTest.scala
+++ src/test/scala/objektwerks/ConfigNativeTest.scala
@@ -1,20 +1,24 @@
 package objektwerks
 
-import zio.Config
+import zio.{Config, ConfigProvider, ZIO}
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 case class JavaHome(value: String)
 
 object JavaHome {
-  val config: Config[JavaHome] = 
-    Config.string("value").map { 
+  def config: Config[JavaHome] = 
+    Config.string("JAVA_HOME").map {
       case value => JavaHome(value)
     }
 }
 
 object ConfigNativeTest extends ZIOSpecDefault:
   def spec = suite("config-native")(
-    test("conf") {
-      assertTrue(true)
+    test("config") {
+      ConfigProvider
+        .envProvider
+        .load(JavaHome.config)
+        .tap(javaHome => ZIO.succeed(javaHome).debug)
+        .map(javaHome => assertTrue(javaHome.value.nonEmpty))
     }
   )
