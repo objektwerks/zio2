4d21433b7f2c3d5821ac26b2f3e7ec0fce17f993
objektwerks <objektwerks@runbox.com>
2022-11-28 12:27:53 -0500

    refactored quill app

--- src/main/scala/objektwerks/QuillApp.scala
+++ src/main/scala/objektwerks/QuillApp.scala
@@ -1,32 +1,36 @@
 package objektwerks
 
+import com.typesafe.config.{Config, ConfigFactory}
+
+import java.sql.SQLException
+
 import zio.{Console, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+
+import io.getquill.*
+import io.getquill.jdbczio.Quill.H2
 
 case class Todo(id: Int = 0, task: String)
 
 case class Store():
-  import com.typesafe.config.{Config, ConfigFactory}
-  import io.getquill.*
-
   val conf = ConfigFactory.load("quill.conf")
-  val ctx = new H2JdbcContext(SnakeCase, conf.getConfig("quill.ctx"))
-  import ctx._
+  val ctx = H2(SnakeCase, new H2JdbcContext(SnakeCase, conf).dataSource)
+  import ctx.*
 
-  inline def addTodo(todo: Todo): Int =
+  inline def addTodo(todo: Todo): ZIO[Any, SQLException, Int] =
     run( 
       query[Todo]
         .insert( lift(todo => (todo.id, todo.task)) )
         .returningGenerated(_.id)
     )
 
-  inline def updateTodo(todo: Todo): Long =
+  inline def updateTodo(todo: Todo): ZIO[Any, SQLException, Long] =
     run(
       query[Todo]
         .filter(_.id == lift(todo.id))
         .update( lift(todo => (todo.id, todo.task)) )
     )
 
-  inline def listTodos: List[Todo] = run( query[Todo] )
+  inline def listTodos: ZIO[Any, SQLException, List[Todo]] = run( query[Todo] )
 
 object QuillApp extends ZIOAppDefault:
   def app: ZIO[Any, Exception, Unit] =
