65e6fe3dc7db79d031b23f722914251f119e2440
objektwerks <objektwerks@runbox.com>
2022-11-28 08:38:46 -0500

    added quill.conf and ddl.sql

---
+++ project/ddl.sql
@@ -0,0 +1,2 @@
+drop table todo if exists;
+create table todo (id int primary key auto_increment, task varchar(128) not null);
--- src/main/scala/objektwerks/QuillApp.scala
+++ src/main/scala/objektwerks/QuillApp.scala
@@ -3,8 +3,23 @@ package objektwerks
 import zio.ZIOAppDefault
 
 import zio.{Console, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import com.typesafe.config.Config
 
 case class Todo(id: Int = 0, task: String)
+
+class Store(conf: Config) {
+  implicit val ctx = new H2JdbcContext(SnakeCase, conf.getConfig("quill.ctx"))
+  import ctx._
+
+  def addTodo(todo: Todo): Int = run( query[Todo].insert(lift(todo)).returningGenerated(_.id) )
+
+  def updateTodo(todo: Todo): Unit = {
+    run( query[Todo].filter(_.id == lift(todo.id)).update(lift(todo)) )
+    ()
+  }
+
+  def listTodos(): Seq[Todo] = run( query[Todo] )
+}
 
 object QuillApp extends ZIOAppDefault:
   def app: ZIO[Any, Exception, Unit] =
