9a7687023e00c0d26adc4f70e2d3215a0317bc9c
objektwerks <objektwerks@runbox.com>
2022-11-28 19:11:23 -0500

    refactored http server

--- src/main/scala/objektwerks/HttpServer.scala
+++ src/main/scala/objektwerks/HttpServer.scala
@@ -13,13 +13,10 @@ import Command.given
 import Event.given
 
 object HttpServer extends ZIOAppDefault:
-  val port = 7272
-  val config = ServerConfig.default.port(port)
-
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> file(Path.of("./target/http-server.log"))
 
-  def router: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
+  val router: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
     case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
     case request @ Method.POST -> !! / "greeting" => request.body.asString.map { name => Response.text(s"\nGreetings, $name!\n") }
     case request @ Method.POST -> !! / "command" => request.body.asString.map { json =>
@@ -33,6 +30,9 @@ object HttpServer extends ZIOAppDefault:
 
   override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
     for
+      args   <- getArgs
+      port   =  args.headOption.getOrElse("7272").toInt
       _      <- ZIO.log(s"HttpServer running at http://localhost:$port")
+      config =  ServerConfig.default.port(port)
       server <- Server.serve(router).provide(ServerConfig.live(config), Server.live)
     yield server
