eab51bc18d38ace3b58e0c1aefb51cbea94176ad
objektwerks <objektwerks@runbox.com>
2022-12-03 07:22:58 -0500

    enhanced cache app

--- src/main/scala/objektwerks/CacheApp.scala
+++ src/main/scala/objektwerks/CacheApp.scala
@@ -10,11 +10,13 @@ object CacheApp extends ZIOAppDefault:
     for
       cache  <- Cache.make(capacity = 10,
                            timeToLive = Duration.Infinity,
-                           lookup = Lookup( (n: Int) => ZIO.debug(s"lookup $n").as(n) ) )
+                           lookup = Lookup( (n: Int) => ZIO.debug(s"cached $n").as(n) ) )
       first  <- cache.get(1)
       _      <- ZIO.debug(s"first get(1): $first")
       second <- cache.get(1)
       _      <- ZIO.debug(s"second get(1): $second")
+      bool   <- cache.contains(1)
+      _      <- ZIO.debug(s"contains(1): $bool")
     yield ()
 
   def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
