95cc487604291e06db58eeae322cd49b82332520
objektwerks <objektwerks@runbox.com>
2022-12-03 07:37:00 -0500

    edited cache app

--- src/main/scala/objektwerks/CacheApp.scala
+++ src/main/scala/objektwerks/CacheApp.scala
@@ -8,13 +8,13 @@ import zio.cache.{Cache, Lookup}
 object CacheApp extends ZIOAppDefault:
   def app =
     for
-      cache  <- Cache.make(capacity = 10,
-                           timeToLive = Duration.Infinity,
-                           lookup = Lookup( (n: Int) => ZIO.debug(s"lookup cached => $n").as(n) ) )
-      put    <- cache.get(1) // lookup caches, or puts, 1 if it doesn't exist in the cache!
-      _      <- ZIO.debug(s"cache.get(1) => $put")
-      bool   <- cache.contains(1)
-      _      <- ZIO.debug(s"cache.contains(1) => $bool")
+      cache    <- Cache.make(capacity = 10,
+                             timeToLive = Duration.Infinity,
+                             lookup = Lookup( (n: Int) => ZIO.debug(s"lookup cached => $n").as(n) ) )
+      put      <- cache.get(1) // lookup caches, or puts, 1 if it doesn't exist in the cache!
+      _        <- ZIO.debug(s"cache.get(1) => $put")
+      contains <- cache.contains(1)
+      _        <- ZIO.debug(s"cache.contains(1) => $contains")
     yield ()
 
   def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app
