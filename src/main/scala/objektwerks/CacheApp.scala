3a051256af233f5161ea763c7a350b44e2eb2d5f
objektwerks <objektwerks@runbox.com>
2022-12-04 12:44:22 -0500

    renamed cache

--- src/main/scala/objektwerks/CacheApp.scala
+++ src/main/scala/objektwerks/CacheApp.scala
@@ -3,28 +3,28 @@ package objektwerks
 import zio.{Console, Duration, Runtime, Scope, UIO, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 import zio.cache.{Cache, Lookup}
 
-case class CacheService(cache: Cache[Int, Nothing, Int]):
+case class IntCache(cache: Cache[Int, Nothing, Int]):
   def get(n: Int): UIO[Int] = cache.get(n)
   def contains(n: Int): UIO[Boolean] = cache.contains(n)
 
-object CacheService:
-  val layer: ZLayer[Any, Nothing, CacheService] =
+object IntCache:
+  val layer: ZLayer[Any, Nothing, IntCache] =
     ZLayer {
       for
         cache <- Cache.make(capacity = 10,
                             timeToLive = Duration.Infinity,
                             lookup = Lookup( (n: Int) => ZIO.debug(s"lookup cached => $n").as(n) ) )
-      yield CacheService(cache)
+      yield IntCache(cache)
     }
 
 object CacheApp extends ZIOAppDefault:
-  def app: ZIO[CacheService, Nothing, Unit] =
+  def app: ZIO[IntCache, Nothing, Unit] =
     for
-      cache    <- ZIO.service[CacheService]
+      cache    <- ZIO.service[IntCache]
       get      <- cache.get(1) // Lookup caches, or puts, 1 if it doesn't exist in the cache!
       _        <- ZIO.debug(s"cache.get(1) => $get")
       contains <- cache.contains(1)
       _        <- ZIO.debug(s"cache.contains(1) => $contains")
     yield () // Make a cache in the layer that constructs your service.
 
-  def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(CacheService.layer)
+  def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(IntCache.layer)
