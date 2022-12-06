92d8e15ace1cc85cdd9a5692261638683c1f890e
objektwerks <objektwerks@runbox.com>
2022-12-06 12:36:24 -0500

    enhanced http route test

--- src/test/scala/objektwerks/HttpRouteTest.scala
+++ src/test/scala/objektwerks/HttpRouteTest.scala
@@ -1,20 +1,27 @@
 package objektwerks
 
 import zio.ZIO
-import zio.http.{!!, /, ->, Http, Request, Response, URL}
+import zio.http.{!!, /, ->, Body, Http, Request, Response, URL}
 import zio.http.model.Method
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 object HttpRouteTest extends ZIOSpecDefault:
-  val route: Http[Any, Nothing, Request, Response] = Http.collectZIO[Request] {
-    case Method.GET -> !! / "test" => ZIO.succeed(Response.text("test"))
+  val route: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
+    case Method.GET -> !! / "get" => ZIO.succeed(Response.text("get"))
+    case request @ Method.POST -> !! / "post" => request.body.asString.map { data => Response.text(data) }
   }
 
-  def spec = suite("http")(
-    test("route") {
-      val request = Request.get(URL(!! / "test"))
+  def spec = suite("http route")(
+    test("get") {
+      val request = Request.get(URL(!! / "get"))
       for
         response <- route(request).flatMap(response => response.body.asString)
-      yield assertTrue(response == "test")
+      yield assertTrue(response == "get")
+    },
+    test("post") {
+      val request = Request.post(Body.fromString("post"), URL(!! / "post"))
+      for
+        response <- route(request).flatMap(response => response.body.asString)
+      yield assertTrue(response == "post")
     }
   )
