ccde64b9243cea6828e5abff7c456c2e7c1cd3d5
objektwerks <objektwerks@runbox.com>
2022-12-08 06:35:58 -0500

    added final to commands

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -4,8 +4,8 @@ import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
 
 sealed trait Command
 
-case class Add(x: Int, y: Int) extends Command
-case class Multiply(x: Int, y: Int) extends Command
+final case class Add(x: Int, y: Int) extends Command
+final case class Multiply(x: Int, y: Int) extends Command
 
 object Command:
   given JsonDecoder[Command] = DeriveJsonDecoder.gen[Command]
