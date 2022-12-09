bbe3767228c88fed42c8d3933e25d75a532653a4
objektwerks <objektwerks@runbox.com>
2022-12-09 17:34:50 -0500

    added quill postgresql app

---
+++ src/main/scala/objektwerks/QuillPostgreSqlApp.scala
@@ -0,0 +1,54 @@
+package objektwerks
+
+import com.typesafe.config.Config
+
+import io.getquill.*
+import io.getquill.jdbczio.Quill.H2
+
+import java.io.IOException
+import java.sql.SQLException
+
+import zio.{Console, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+
+final case class PostgreSqlStore(quill: H2[SnakeCase]):
+  import quill.*
+
+  def addTodo(todo: Todo): ZIO[Any, SQLException, Int] =
+    run( 
+      query[Todo]
+        .insertValue( lift(todo) )
+        .returningGenerated(_.id)
+    )
+
+  def updateTodo(todo: Todo): ZIO[Any, SQLException, Long] =
+    run(
+      query[Todo]
+        .filter(_.id == lift(todo.id))
+        .updateValue( lift(todo) )
+    )
+
+  def listTodos: ZIO[Any, SQLException, List[Todo]] = run( query[Todo] )
+
+object PostgreSqlStore:
+  val layer: ZLayer[Any, IOException, PostgreSqlStore] =
+    ZLayer {
+      for
+        config <- Resources.loadConfig(path = "quill.conf", section = "pgsql")
+      yield PostgreSqlStore(H2(SnakeCase, PostgresJdbcContext(SnakeCase, config).dataSource))
+    }
+
+object QuillPostgreSqlApp extends ZIOAppDefault:
+  def app: ZIO[PostgreSqlStore, Exception, Unit] =
+    for
+      store <- ZIO.service[PostgreSqlStore]
+      id    <- store.addTodo( Todo(task = "mow yard") )
+      _     <- Console.printLine(s"Todo id: $id")
+      todos <- store.listTodos
+      _     <- Console.printLine(s"Todos: $todos")
+      ct    <- store.updateTodo( todos(0).copy(task = "mowed yard") )
+      _     <- Console.printLine(s"Update count: $ct")
+      dones <- store.listTodos
+      _     <- Console.printLine(s"Dones: $dones")
+    yield ()
+
+  def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] = app.provide(PostgreSqlStore.layer)
