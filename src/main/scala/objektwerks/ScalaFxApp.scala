0e41170ea272a14735b8f559692166fd691e2966
objektwerks <objektwerks@runbox.com>
2022-12-13 16:14:28 -0500

    enhanced scalafx app

--- src/main/scala/objektwerks/ScalaFxApp.scala
+++ src/main/scala/objektwerks/ScalaFxApp.scala
@@ -4,6 +4,7 @@ import scalafx.Includes._
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.control.{ListView, TextField}
+import scalafx.scene.layout.VBox
 
 import zio.{Chunk, Console, Runtime, Unsafe, ZIO}
 import zio.stream.ZStream
@@ -12,6 +13,7 @@ object ScalaFxApp extends JFXApp3:
   override def start(): Unit =
     val textField = new TextField()
     val listView = new ListView[String]()
+    val vbox = new VBox(textField, listView)
 
     val zioStream = ZStream.async[Any, Any, String] { emitter =>
       textField.text.onChange { (_, _, newValue) =>
@@ -28,7 +30,7 @@ object ScalaFxApp extends JFXApp3:
 
     stage = new JFXApp3.PrimaryStage {
       scene = new Scene {
-        content = textField
+        content = vbox
       }
     }
 
