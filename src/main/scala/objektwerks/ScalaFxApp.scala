ddbdb50b39be7507f0b82a58fb1bc3760268e7d2
objektwerks <objektwerks@runbox.com>
2022-12-14 12:29:56 -0500

    edited comment

--- src/main/scala/objektwerks/ScalaFxApp.scala
+++ src/main/scala/objektwerks/ScalaFxApp.scala
@@ -13,6 +13,14 @@ import zio.stream.ZStream
 
 /**
   * See alternate implementation by Duran: https://pastebin.com/ErsB0rkc
+  * 
+  * This dependency: compilerPlugin("com.github.ghik" % "zerowaste" % "0.2.1" cross CrossVersion.full)
+  * produces this error: discarded expression with non-Unit value
+  * 
+  * With standard ZIO code this is a good thing. With ScalaFx event listeners it's an annoyance.
+  * 
+  * The current solution is employ this approach: val _ = ZIO code here! See 2 examples below.
+  * 
   */
 object ScalaFxApp extends JFXApp3:
   def shouldBeInFxThread(message: String): Unit = require(Platform.isFxApplicationThread, message)
