fe3bccaa406bf5334267c05cd4b810871e3fe114
objektwerks <objektwerks@runbox.com>
2024-08-28 08:37:18 -0400

    corrected issues with system app

--- src/main/scala/objektwerks/SystemApp.scala
+++ src/main/scala/objektwerks/SystemApp.scala
@@ -1,15 +1,11 @@
 package objektwerks
 
 import zio.{Console, Runtime, Scope, System, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
-import zio.logging.console
 
 /**
   * System.env("JAVA_HOME") fails in a ZIO test. Why?
   */
 object SystemApp extends ZIOAppDefault:
-  override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
-    Runtime.removeDefaultLoggers >>> console()
-
   def app: ZIO[Any, Exception, Unit] =
     for
       opt <- System.env("JAVA_HOME")
