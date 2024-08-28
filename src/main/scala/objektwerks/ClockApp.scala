550959b930c22a2c0dd0c412ee2eff53b9d7abda
objektwerks <objektwerks@runbox.com>
2024-08-28 08:33:41 -0400

    corrected console app

--- src/main/scala/objektwerks/ClockApp.scala
+++ src/main/scala/objektwerks/ClockApp.scala
@@ -3,12 +3,8 @@ package objektwerks
 import java.io.IOException
 
 import zio.{Clock, Console, Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
-import zio.logging.console
 
 object ClockApp extends ZIOAppDefault:
-  override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
-    Runtime.removeDefaultLoggers >>> console()
-
   def app: ZIO[Any, IOException, Unit] =
     for
       dt <- Clock.currentDateTime
