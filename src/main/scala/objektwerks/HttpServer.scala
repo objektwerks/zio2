f0b50fd6053d916eb04317a1e46b2588ea1d8112
objektwerks <objektwerks@runbox.com>
2024-08-28 08:44:35 -0400

    edited http server

--- src/main/scala/objektwerks/HttpServer.scala
+++ src/main/scala/objektwerks/HttpServer.scala
@@ -4,15 +4,15 @@ import java.nio.file.Path
 import java.time.Instant
 
 import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
-import zio.http.{Request, Response, Server}
+import zio.http.{Charsets, handler, Method, Request, Response, Routes}
 import zio.json.{DecoderOps, EncoderOps}
 
 import Command.given
 import Event.given
 
 object HttpServer extends ZIOAppDefault:
-  val route: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
-    case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
+  val route = Routes(
+    Method.GET -> / "now" -> ZIO.succeed( Response.text(Instant.now.toString()) )
     case request @ Method.POST -> !! / "greeting" => request.body.asString.map { name => Response.text(s"\nGreetings, $name!\n") }
     case request @ Method.POST -> !! / "command" => request.body.asString.map { json =>
       json.fromJson[Command] match
@@ -21,7 +21,7 @@ object HttpServer extends ZIOAppDefault:
           case Multiply(x, y) => Response.json( Muliplied( x * y ).toJson )
         case Left(error) => Response.json( Fault( error ).toJson )
     }
-  }
+  )
 
   override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
     for
